722b177ebc99c384db13d962113017ef137fac20
Anti614 <julianikulina55@gmail.com>
2021-09-13 18:02:36 -0400

    refactor assets

--- js/main.js
+++ js/main.js
@@ -5,5 +5,22 @@ $(document).ready(function(){
     itemSelector: '.tab-pane__item',
     fitWidth: true,
     gutter: 14
-});
   });
+
+  $("[data-value]").on("click", function(event) { 
+    event.preventDefault();  
+    
+    let elementId = $(this).data('value');
+    
+    /*let elementOffset = $(elementId).offset().top;
+    nav.removeClass("show");*/
+
+
+    /*$("html, body").animate ({
+       scrollTop: elementOffset - 70
+    }, 700);*/
+
+ });
+
+
+});
